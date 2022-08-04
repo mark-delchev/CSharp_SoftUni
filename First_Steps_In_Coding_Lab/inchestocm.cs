46046a60ac3d83e4bdb0d2f1e11b055559bdd16f
Mark Delchev <markdelchev@gmail.com>
2022-08-04 11:27:31 +0300

    Add files via upload

---
+++ First_Steps_In_Coding_Lab/inchestocm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace inchestocm
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = 2.54;
+            Console.WriteLine(a * b);
+        }
+    }
+}
