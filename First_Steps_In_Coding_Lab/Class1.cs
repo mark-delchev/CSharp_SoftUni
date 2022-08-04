423962c24a1699639a1f6ff8c8ff141462c4a3c3
Mark Delchev <markdelchev@gmail.com>
2022-08-04 11:11:01 +0300

    Add files via upload

---
+++ First_Steps_In_Coding_Lab/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+}
+}
