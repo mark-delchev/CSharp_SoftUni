36e9c47d84b51053fbb9e889081dd547c439dce9
Mark Delchev <markdelchev@gmail.com>
2022-08-05 18:29:35 +0300

    Add files via upload

---
+++ First_Steps_In_Coding_Lab/yard_greening.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Yard
+{
+    class Progeam
+    {
+        static void Main(string[] args)
+        {
+            double square_meters = double.Parse(Console.ReadLine());
+            double price = square_meters * 7.61;
+            double discount = price * 0.18;
+            double final_price = price - discount;
+            Console.WriteLine($"The final price is: {final_price} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
