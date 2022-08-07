78074806606e0b1a0b1552284b96561f496db08c
Mark Delchev <markdelchev@gmail.com>
2022-08-07 11:51:46 +0300

    Add files via upload

---
+++ First_Steps_In_Coding_Exercise/fish_tank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HEEEEE
+{
+    class HHHHHHHH
+    {
+        static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine()) / 100;
+            double liters = ((length * width * height) / 1000) * (1 - percentage);
+            Console.WriteLine(liters);
+        }
+    }
+}
