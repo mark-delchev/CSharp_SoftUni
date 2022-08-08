ba1f747efac2ae532a1f1f4598f7b529aa8c1ac3
Mark Delchev <markdelchev@gmail.com>
2022-08-08 11:37:06 +0300

    Add files via upload

---
+++ Programming_Basics/Conditional_Statements_Lab/area_of_figures.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Area
+{
+    class Figures
+    {
+        static void Main()
+        {
+            string figure = Console.ReadLine();
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{side * side:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{side1 * side2:F3}");
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{Math.PI * radius * radius:F3}");
+            }
+            else
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{(side1 * side2) / 2:F3}");
+            }
+        }
+    }
+}
