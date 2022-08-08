b05b29e7ba8f6201d7dcc8ac7242be2b0f5b4be2
Mark Delchev <markdelchev@gmail.com>
2022-08-08 10:43:25 +0300

    Add files via upload

---
+++ Programming_Basics/Conditional_Statements_Lab/excellent_result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Results
+{
+    class Excellent
+    {
+        static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
