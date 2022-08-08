6e5ebddb66a0b4d9779fc2d9acc06f8150d32030
Mark Delchev <markdelchev@gmail.com>
2022-08-08 10:52:53 +0300

    Add files via upload

---
+++ Programming_Basics/Conditional_Statements_Lab/password_guess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Password
+{
+    class Guess
+    {
+        static void Main()
+        {
+            string password = Console.ReadLine();
+            if (password == "s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
