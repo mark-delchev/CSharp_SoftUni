6d43b31e05731be655232460abdd64025d819318
Mark Delchev <markdelchev@gmail.com>
2022-08-08 10:48:20 +0300

    Add files via upload

---
+++ Programming_Basics/Conditional_Statements_Lab/even_or_odd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Even
+{
+    class Odd
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
