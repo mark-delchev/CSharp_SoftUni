1dc79638b52cffc5916d9379c14322ee0c1cfb36
Mark Delchev <markdelchev@gmail.com>
2022-08-23 13:16:01 +0300

    time+15

---
+++ Programming_Basics/Conditional_Statements_Exercise/time+15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Programs
+{
+    class Hello
+    {
+        static void Main()
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int time = hours * 60 + minutes;
+            int time_15 = time + 15;
+            int hour_15 = time_15 / 60;
+            int minutes_15 = time_15 % 60;
+            if (hour_15 > 23)
+            {
+                hour_15 = 0;
+            }
+            if (minutes_15 < 10)
+            {
+                Console.WriteLine($"{hour_15}:0{minutes_15}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour_15}:{minutes_15}");
+            }
+        }
+    }
+}
